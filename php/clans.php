576eeddc3459424aeced3b90090c67ee26a3be2c
Mar-Kno <marius.knotz@t-online.de>
2022-05-06 13:22:54 +0200

    feat: edit clans

--- php/clans.php
+++ php/clans.php
@@ -1,3 +1,10 @@
-<p>
-    clans 
-</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mandalore - Clans</title>
+</head>
+<body>
+    <h1>Clans</h1>
+</body>
+</html>
