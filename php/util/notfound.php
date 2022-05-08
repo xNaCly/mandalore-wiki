028d40ec8355a82d0d5a99bdb794466b02f8718e
xnacly <matteogropp@protonmail.com>
2022-05-08 18:51:58 +0200

    feat: error document & removed file ext in url

---
+++ php/util/notfound.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mandalore - 
+    <?php 
+        include "./util.php";
+        echo format_title(__FILE__);
+    ?>
+    </title>
+    <link rel="stylesheet" href="../../css/style.css" type="text/css">
+</head>
+<body>
+    <h1>404 - requested site not found</h1>
+</body>
+</html>
