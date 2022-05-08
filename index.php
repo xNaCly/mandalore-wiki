9f49435d969a89f6f0f0fd6a35cf1747739cfb1d
xnacly <matteogropp@protonmail.com>
2022-05-08 18:52:19 +0200

    refactor: moved util to seperate dir

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Mandalore - Wiki</title>
+  <title>Mandalore - 
+    <?php 
+        include "./php/util/util.php";
+        echo format_title(__FILE__);
+    ?>
+    </title>
   <link rel="stylesheet" href="./css/style.css" type="text/css">
 </head>
 <body>
@@ -10,6 +15,6 @@
     include "./php/templates/header.php";
     include "./php/style-showcase.php";
   ?>
-  <a href="./clans">clans</a>
+  <a href="./clans.php">clans</a>
 </body>
 </html>
--- php/util/util.php
+++ php/util/util.php
@@ -4,6 +4,13 @@ function format_title($file_path){
     $file_name_with_ext = basename($file_path);
     // split file_name_wit_ext into 'file_name' and 'php'
     $file_name = explode(".", $file_name_with_ext)[0];
+
+    if (strcmp($filename, "index")) {
+        $file_name = "Wiki";
+    } else if (strcmp($file_name, "notfound")){
+        $file_name = "404";
+    }
+    
     // convert filename to titlecase: 'file_name' -> 'File_name'
     $page_name = ucwords($file_name);
     return $page_name;
