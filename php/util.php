2c4e2b065a895567cbada11cfacc752e92ea73d5
xnacly <matteogropp@protonmail.com>
2022-05-08 15:06:23 +0200

    feat(util): add snippet to dynamically write site title

---
+++ php/util.php
@@ -0,0 +1,11 @@
+<?php
+function format_title($file_path){
+    // get *.php from file_path
+    $file_name_with_ext = basename($file_path);
+    // split file_name_wit_ext into 'file_name' and 'php'
+    $file_name = explode(".", $file_name_with_ext)[0];
+    // convert filename to titlecase: 'file_name' -> 'File_name'
+    $page_name = ucwords($file_name);
+    return $page_name;
+}
+?>
