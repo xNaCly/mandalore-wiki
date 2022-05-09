85b3d9fd217c58f168ed25bf3d765ff933c0c39d
xnacly <matteogropp@protonmail.com>
2022-05-09 12:41:16 +0200

    feat(404): updated text - #5

--- php/util/notfound.php
+++ php/util/notfound.php
@@ -13,8 +13,10 @@
 <body>
     <div class="container container-flex">
         <h1 class="error-heading">404</h1>
-        <h3>Requested Site not found</h3>
-        <a href="/">Home</a>
+        <h3>Ooops, you've hyperjumped too far into the unknown regions.</h3>
+        <h4>We recommend taking a hyperspace route back to Mandalore.</h4>
+        <h4>May the force be with you!</h4>
+        <a href="/">Jump to Mandalore</a>
     </div>
 </body>
 </html>
