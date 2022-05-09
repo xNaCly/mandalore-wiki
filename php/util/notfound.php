ebd662d6e1922167c539f24a7a5f248c57e68f2f
xnacly <matteogropp@protonmail.com>
2022-05-09 12:17:34 +0200

    feat: add#5, move header to templates

--- php/util/notfound.php
+++ php/util/notfound.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" href="../../css/style.css" type="text/css">
 </head>
 <body>
-    <h1>404 - requested site not found</h1>
+    <div class="container container-flex">
+        <h1 class="error-heading">404</h1>
+        <h3>Requested Site not found</h3>
+        <a href="/">Home</a>
+    </div>
 </body>
 </html>
