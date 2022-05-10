87c2ca47adaea3c1a9b8897e8882ce30021f4319
xnacly <matteogropp@protonmail.com>
2022-05-10 19:04:30 +0200

    style: changed formatting

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Mandalore - 
-    <?php 
-        include "./php/util/util.php";
-        echo format_title(__FILE__);
-    ?>
-    </title>
+  <title>Mandalore -
     <?php
-        include "./php/templates/header.php"
+    include "./php/util/util.php";
+    echo format_title(__FILE__);
     ?>
+  </title>
+  <?php
+  include "./php/templates/header.php"
+  ?>
 </head>
+
 <body>
   <?php
-    include "./php/templates/navbar.php";
-    include "./php/style-showcase.php";
-    include "./php/style-showcase.php";
-    include "./php/style-showcase.php";
+  include "./php/templates/navbar.php";
+  include "./php/style-showcase.php";
+  include "./php/style-showcase.php";
+  include "./php/style-showcase.php";
   ?>
   <a href="./clans.php">clans</a>
 </body>
+
 </html>
--- php/util/notfound.php
+++ php/util/notfound.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Mandalore - 
-    <?php 
-        include "./util.php";
-        echo format_title(__FILE__);
-    ?>
-    </title>
-    <link rel="stylesheet" href="../../css/style.css" type="text/css">
-    <link rel="stylesheet" href="../../css/fonts.css" type="text/css">
-</head>
-<body>
+  <meta charset="UTF-8">
+  <title>Mandalore -
     <?php
-    include "../templates/navbar.php"
+    include "./util.php";
+    echo format_title(__FILE__);
     ?>
-    <div class="container container-flex">
-        <h1 class="error-heading">* 404</h1>
-        <h3>Ooops, you've hyperjumped too far into the unknown regions.</h3>
-        <h4>We recommend taking a hyperspace route back to Mandalore.</h4>
-        <h4>May the force be with you!</h4>
-        <a href="/">Jump to Mandalore</a>
-    </div>
+  </title>
+  <link rel="stylesheet" href="../../css/style.css" type="text/css">
+  <link rel="stylesheet" href="../../css/fonts.css" type="text/css">
+</head>
+
+<body>
+  <?php
+  include "../templates/navbar.php"
+  ?>
+  <div class="container container-flex">
+    <h1 class="error-heading">* 404</h1>
+    <h3>Ooops, you've hyperjumped too far into the unknown regions.</h3>
+    <h4>We recommend taking a hyperspace route back to Mandalore.</h4>
+    <h4>May the force be with you!</h4>
+    <a href="/">Jump to Mandalore</a>
+  </div>
 </body>
+
 </html>
