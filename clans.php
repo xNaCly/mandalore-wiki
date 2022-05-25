f6ed2d3551d059aa4a42d3d264331de1f4cd1152
Mar-Kno <marius.knotz@t-online.de>
2022-05-25 14:49:37 +0200

    edit: clans img,details

--- clans.php
+++ clans.php
@@ -15,7 +15,111 @@
     <?php
     include "./php/templates/navbar.php";
     ?>
-    <h1>Clans</h1>
+    <h1><b>Clans</b></h1>
     <h3>Clan Fett, Clan Vizsla, Clan Wren, Clan Saxon</h3>
+
+    <div class="clan-logo-container">
+        <div class="clan-logo-subcontainer">
+            <span class="clan-logo-name ft-bold">Clan Fett</span>
+            <img src="assets\images\clans\images_clans_fett.png" alt="clan fett"
+            class="clan-logo">
+            <details>
+                <summary class="clan-logo-summary-text">Details anzeigen</summary>
+                <p >
+                    Clan Fett was a Mandalorian Clan that consisted <br>
+                    of some of the most infamous members of the <br>
+                    Mandalorian culture to exist. Members of the <br>
+                    clan were known for their tactical superiority <br>
+                    and for being skilled warriors. At some point <br>
+                    between the end of the Mandalorian Wars and the <br>
+                    start of the Mandalorian Civil War, a majority <br>
+                    of Clan Fett's members were either wiped out <br>
+                    or had abandoned the Mandalorian ways. <br>
+                    Eventually, though, some members of Clan Fett <br>
+                    would find their way back into Mandalorian <br>
+                    culture, such as Jango Fett, who was adopted <br>
+                    by Mand'alor, Jaster Mereel after his parents <br>
+                    were murdered by Death Watch.<br>
+                </p>
+            </details>
+        </div>
+        <div class="clan-logo-subcontainer">
+            <span class="clan-logo-name ft-bold">Clan Vizsla</span>
+            <img src="assets\images\clans\images_clans_vizsla.png" alt="clan vizsla"
+            class="clan-logo">
+            <details>
+                <summary class="clan-logo-summary-text">Details anzeigen</summary>
+                <p >
+                    Clan Fett was a Mandalorian Clan that consisted <br>
+                    of some of the most infamous members of the <br>
+                    Mandalorian culture to exist. Members of the <br>
+                    clan were known for their tactical superiority <br>
+                    and for being skilled warriors. At some point <br>
+                    between the end of the Mandalorian Wars and the <br>
+                    start of the Mandalorian Civil War, a majority <br>
+                    of Clan Fett's members were either wiped out <br>
+                    or had abandoned the Mandalorian ways. <br>
+                    Eventually, though, some members of Clan Fett <br>
+                    would find their way back into Mandalorian <br>
+                    culture, such as Jango Fett, who was adopted <br>
+                    by Mand'alor, Jaster Mereel after his parents <br>
+                    were murdered by Death Watch.<br>
+                </p>
+            </details>
+        </div>
+        
+        <div class="clan-logo-subcontainer">
+            <span class="clan-logo-name ft-bold">Clan Wren</span>
+            <img src="assets\images\clans\images_clans_fett.png" alt="clan fett"
+            class="clan-logo">
+            <details>
+                <summary class="clan-logo-summary-text">Details anzeigen</summary>
+                <p >
+                    Clan Fett was a Mandalorian Clan that consisted <br>
+                    of some of the most infamous members of the <br>
+                    Mandalorian culture to exist. Members of the <br>
+                    clan were known for their tactical superiority <br>
+                    and for being skilled warriors. At some point <br>
+                    between the end of the Mandalorian Wars and the <br>
+                    start of the Mandalorian Civil War, a majority <br>
+                    of Clan Fett's members were either wiped out <br>
+                    or had abandoned the Mandalorian ways. <br>
+                    Eventually, though, some members of Clan Fett <br>
+                    would find their way back into Mandalorian <br>
+                    culture, such as Jango Fett, who was adopted <br>
+                    by Mand'alor, Jaster Mereel after his parents <br>
+                    were murdered by Death Watch.<br>
+                </p>
+            </details>
+        </div>
+        
+        <div class="clan-logo-subcontainer">
+            <span class="clan-logo-name ft-bold">Clan Saxon</span>
+            <img src="assets\images\clans\images_clans_fett.png" alt="clan fett"
+            class="clan-logo">
+            <div>
+                <details>
+                    <summary class="clan-logo-summary-text">Details anzeigen</summary>
+                    <p >
+                        Clan Fett was a Mandalorian Clan that consisted <br>
+                        of some of the most infamous members of the <br>
+                        Mandalorian culture to exist. Members of the <br>
+                        clan were known for their tactical superiority <br>
+                        and for being skilled warriors. At some point <br>
+                        between the end of the Mandalorian Wars and the <br>
+                        start of the Mandalorian Civil War, a majority <br>
+                        of Clan Fett's members were either wiped out <br>
+                        or had abandoned the Mandalorian ways. <br>
+                        Eventually, though, some members of Clan Fett <br>
+                        would find their way back into Mandalorian <br>
+                        culture, such as <mark>Jango Fett</mark>, who was adopted <br>
+                        by Mand'alor, Jaster Mereel after his parents <br>
+                        were murdered by Death Watch.<br>
+                    </p>
+                </details>
+            </div>
+        </div>
+    </div>
+        
 </body>
 </html>
