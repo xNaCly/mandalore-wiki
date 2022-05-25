a70d87929e2eae153a96f15ff84219476dd46342
xnacly <matteogropp@protonmail.com>
2022-05-25 12:16:49 +0200

    docs: imporve jsdoc for util methods

--- php/util/debug.php
+++ php/util/debug.php
@@ -1,5 +1,11 @@
 <?php
 
-function debug($text){
-    echo "<script>console.log('$text');</script>";
+/**
+ * json encodes '$text' and logs to browser console by injecting a script tag into the DOM
+ * @return void
+ */
+function debug(mixed $text)
+{
+  $text = json_encode($text);
+  echo "<script>console.log('$text');</script>";
 }
