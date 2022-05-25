c3946745774628e521c8888c1b2f14b87e30a6f2
xnacly <matteogropp@protonmail.com>
2022-05-25 12:16:49 +0200

    docs: imporve jsdoc for util methods

--- php/util/util.php
+++ php/util/util.php
@@ -1,11 +1,26 @@
 <?php
-function format_title($file_path)
+
+/**
+ * dynamically change the current window title to the filename without extension.
+ * function should be called like so in the head of a file:
+ * ```html
+ *    <title>Mandalore -
+ *      <?php
+ *        include "./php/util/util.php";
+ *        echo format_title(__FILE__);
+ *      ?>
+ *     </title>
+ * ```
+ * @return string formatted title, for clans.php: Clans
+ */
+function format_title(string $file_path)
 {
   // get *.php from file_path
   $file_name_with_ext = basename($file_path);
   // split file_name_wit_ext into 'file_name' and 'php'
   $file_name = explode(".", $file_name_with_ext)[0];
 
+  // custom title for some pages
   if (strcmp($file_name, "index") == 0) {
     $file_name = "wiki";
   } else if (strcasecmp($file_name, "notfound") == 0) {
