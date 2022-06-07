41fe3be8c703e3f6f2b010f86374113a0dcc77f4
xnacly <matteogropp@protonmail.com>
2022-06-07 10:20:19 +0200

    feat: add alt attribute to homepage image

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <title>Mandalore -
+  <head>
+    <title>Mandalore -
+      <?php
+      include "./php/util/util.php";
+      echo format_title(__FILE__);
+      ?>
+    </title>
     <?php
-    include "./php/util/util.php";
-    echo format_title(__FILE__);
+      include "./php/templates/header.php"
     ?>
-  </title>
-  <?php
-  include "./php/templates/header.php"
-  ?>
-</head>
+  </head>
 
-<body>
-  <?php
-  include "./php/templates/navbar.php";
-  echo gen_navbar(__FILE__);
-  ?>
-  <div class="container container-flex homepage-container">
-    <div class="homepage-main-image-container">
-      <img class="box homepage-main-image" src="./assets/images/din-fighting.webp">
-      <span class="homepage-main-image-text ft-bold">Mandalore Wiki</span>
+  <body>
+    <?php
+      include "./php/templates/navbar.php";
+      echo gen_navbar(__FILE__);
+    ?>
+    <div class="container container-flex homepage-container">
+      <div class="homepage-main-image-container">
+        <img class="box homepage-main-image" alt="the mandalorian fighting stormtroopers" src="./assets/images/din-fighting.webp">
+        <span class="homepage-main-image-text ft-bold">Mandalore Wiki</span>
+      </div>
     </div>
-  </div>
-</body>
-
+  </body>
 </html>
