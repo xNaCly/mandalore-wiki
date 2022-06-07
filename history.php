85f6a0614f537a8c8b0fcc9aab612a577c080cb4
derPhilosoff <sebastian-wagemann@web.de>
2022-06-07 09:33:57 +0200

    #11

---
+++ history.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <title>Mandalore -
+    <?php
+    include "./php/util/util.php";
+    echo format_title(__FILE__);
+    ?>
+    </title>
+    <?php
+  include "./php/templates/header.php"
+  ?>
+</head>
+
+<body>
+    <?php
+    include "./php/templates/navbar.php";
+    echo gen_navbar(__FILE__);
+    ?>
+    <h1 class="ft-bold">History</h1>
+    <h3>Ancient Mandalore</h3>
+    <h3>Mandalorian Civil War</h3>
+    <h3>Independence</h3>
+    <h3>Deathwatch</h3>
+</body>
+</html>
