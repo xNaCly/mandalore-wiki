b098ab8f83136ccb25892e1108de54552433e204
xnacly <matteogropp@protonmail.com>
2022-06-07 10:18:32 +0200

    refactor: moved 404 page to root

--- 404.php
+++ 404.php
@@ -5,12 +5,13 @@
   <meta charset="UTF-8">
   <title>Mandalore -
     <?php
-    include "./util.php";
+    include "./php/util/util.php";
     echo format_title(__FILE__);
     ?>
   </title>
-  <link rel="stylesheet" href="../../css/style.css" type="text/css">
-  <link rel="stylesheet" href="../../css/fonts.css" type="text/css">
+  <?php
+    include "./php/templates/header.php";
+  ?>
 </head>
 
 <body>
@@ -19,7 +20,7 @@
     <h3>Ooops, you've hyperjumped too far into the unknown regions.</h3>
     <h4>We recommend taking a hyperspace route back to Mandalore.</h4>
     <h4>May the force be with you!</h4>
-    <a href="/">Jump to Mandalore</a>
+    <a href="./">Jump to Mandalore</a>
   </div>
 </body>
 
