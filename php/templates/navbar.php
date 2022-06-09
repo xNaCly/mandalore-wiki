3f58d9dc5c940e1f3a4c53016b846e4b19f7c67a
xnacly <matteogropp@protonmail.com>
2022-06-09 09:26:40 +0200

    fix(nav): fixed navbar missing import

--- php/templates/navbar.php
+++ php/templates/navbar.php
@@ -1,6 +1,4 @@
 <?php
-include "../util/util.php";
-
 /**
  * generates navbar by looping over a defined array of sites and inserting them in the navbar. the current page is given the id 'active'
  */
@@ -11,6 +9,14 @@ function gen_navbar(string $file_path)
   $sites = array("clans", "history");
 
   foreach ($sites as $site) {
+    $file_name_with_ext = basename($file_path);
+    $file_name = explode(".", $file_name_with_ext)[0];
+    if (strcmp($file_name, "index") == 0) {
+      $file_name = "wiki";
+    } else if (strcasecmp($file_name, "notfound") == 0) {
+      $file_name = "404";
+    }
+    $page_name = ucwords($file_name);
     $file_name = format_title($file_path);
     $id = "";
     if (strcasecmp($file_name, $site) == 0) {
