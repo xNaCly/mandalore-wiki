65d8e04322f1bb7bf6b86ce9bad220944f107ac4
xnacly <matteogropp@protonmail.com>
2022-07-08 10:51:03 +0200

    fix: made timeline responsive - set overflow-x: scroll for the timeline table - change .card-container-home to .card-container in alignment of child divs in cards and in media query - finish layout for timeline cards

--- timeline.php
+++ timeline.php
@@ -21,185 +21,187 @@
   <div class="container container-flex">
     <h1>Timeline of Mandalorian Content</h1>
     <h2>Timeline:</h2>
-    <table class="timeline-table">
-      <caption>
-        Zeitleiste der für Mandalore wichtigen Filme und Serien im <em>Star Wars</em> Universum
-      </caption>
-      <tbody>
-        <tr>
-          <th class="timeline-heading" colspan="2" rowspan="3">Filme und Serien</th>
-          <th class="timeline-heading" colspan="46">Fiktive Zeitrechnung in Jahren</th>
-        </tr>
-        <tr>
-          <th class="timeline-heading" colspan="28">Vor der Schlacht um Yavin 4</th>
-          <th class="timeline-heading" colspan="18">Nach der Schlacht um Yavin 4</th>
-        </tr>
-        <tr class="timeline-years">
-          <th>33</th>
-          <th>32</th>
-          <th>31</th>
-          <th>30-24</th>
-          <th>23</th>
-          <th>22</th>
-          <th>21</th>
-          <th>20</th>
-          <th>19</th>
-          <th>18</th>
-          <th>17-15</th>
-          <th>14</th>
-          <th>13</th>
-          <th>12</th>
-          <th>11</th>
-          <th>10</th>
-          <th>9</th>
-          <th>8</th>
-          <th>7</th>
-          <th>6</th>
-          <th>5</th>
-          <th>4</th>
-          <th>3</th>
-          <th>2</th>
-          <th>1</th>
-          <th>0</th>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-          <th>4</th>
-          <th>5</th>
-          <th>6</th>
-          <th>7</th>
-          <th>8</th>
-          <th>9</th>
-          <th>10</th>
-          <th>11-31</th>
-          <th>32</th>
-          <th>33</th>
-          <th>34</th>
-          <th>35</th>
-          <th>36</th>
-        </tr>
-        <tr>
-          <td class="timeline-heading">Angriff der Klonkrieger</td>
-          <td rowspan="1" class="legende-atc"></td>
-          <td rowspan="5"></td>
-          <td rowspan="5"></td>
-          <td rowspan="5"></td>
-          <td rowspan="5"></td>
-          <td rowspan="5"></td>
-          <td class="legende-atc">II</td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-        </tr>
-        <tr>
-          <td class="timeline-heading">The Clone Wars</td>
-          <td rowspan="1" class="legende-tcw"></td>
-          <td colspan="4" class="legende-tcw">TCW</td>
-        </tr>
-        <tr>
-          <td class="timeline-heading">Rebels</td>
-          <td rowspan="1" class="legende-rebels"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td rowspan="3"></td>
-          <td colspan="5" class="legende-rebels">Rebels</td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-          <td rowspan="1"></td>
-        </tr>
-        <tr>
-          <td class="timeline-heading">The Mandalorian</td>
-          <td rowspan="1" class="legende-mandalorian"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td class="legende-mandalorian">Mando</td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-          <td rowspan="2"></td>
-        </tr>
-        <tr>
-          <td class="timeline-heading">Book of Boba Fett</td>
-          <td rowspan="1" class="legende-bobf"></td>
-          <td class="legende-bobf">Boba</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="timeline-container">
+      <table class="timeline-table">
+        <caption>
+          Zeitleiste der für Mandalore wichtigen Filme und Serien im <em>Star Wars</em> Universum
+        </caption>
+        <tbody>
+          <tr>
+            <th class="timeline-heading" colspan="2" rowspan="3">Filme und Serien</th>
+            <th class="timeline-heading" colspan="46">Fiktive Zeitrechnung in Jahren</th>
+          </tr>
+          <tr>
+            <th class="timeline-heading" colspan="28">Vor der Schlacht um Yavin 4</th>
+            <th class="timeline-heading" colspan="18">Nach der Schlacht um Yavin 4</th>
+          </tr>
+          <tr class="timeline-years">
+            <th>33</th>
+            <th>32</th>
+            <th>31</th>
+            <th>30-24</th>
+            <th>23</th>
+            <th>22</th>
+            <th>21</th>
+            <th>20</th>
+            <th>19</th>
+            <th>18</th>
+            <th>17-15</th>
+            <th>14</th>
+            <th>13</th>
+            <th>12</th>
+            <th>11</th>
+            <th>10</th>
+            <th>9</th>
+            <th>8</th>
+            <th>7</th>
+            <th>6</th>
+            <th>5</th>
+            <th>4</th>
+            <th>3</th>
+            <th>2</th>
+            <th>1</th>
+            <th>0</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>10</th>
+            <th>11-31</th>
+            <th>32</th>
+            <th>33</th>
+            <th>34</th>
+            <th>35</th>
+            <th>36</th>
+          </tr>
+          <tr>
+            <td class="timeline-heading">Angriff der Klonkrieger</td>
+            <td rowspan="1" class="legende-atc"></td>
+            <td rowspan="5"></td>
+            <td rowspan="5"></td>
+            <td rowspan="5"></td>
+            <td rowspan="5"></td>
+            <td rowspan="5"></td>
+            <td class="legende-atc">II</td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+          </tr>
+          <tr>
+            <td class="timeline-heading">The Clone Wars</td>
+            <td rowspan="1" class="legende-tcw"></td>
+            <td colspan="4" class="legende-tcw">TCW</td>
+          </tr>
+          <tr>
+            <td class="timeline-heading">Rebels</td>
+            <td rowspan="1" class="legende-rebels"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td rowspan="3"></td>
+            <td colspan="5" class="legende-rebels">Rebels</td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+            <td rowspan="1"></td>
+          </tr>
+          <tr>
+            <td class="timeline-heading">The Mandalorian</td>
+            <td rowspan="1" class="legende-mandalorian"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td class="legende-mandalorian">Mando</td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+            <td rowspan="2"></td>
+          </tr>
+          <tr>
+            <td class="timeline-heading">Book of Boba Fett</td>
+            <td rowspan="1" class="legende-bobf"></td>
+            <td class="legende-bobf">Boba</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <div class="container">
       <div class="card-container card-container-timeline">
         <article class="card flex-column bg-gray-700 gray-300">
@@ -215,7 +217,7 @@
             <img class="image" src="assets/images/klonkriege.jpg" alt="angriff der klonkrieger">
           </div>
           <span>
-            Die erste animierte Serie im Star Wars Universium erklärt politische Zusammenhänge bezüglich Mandalore und die kriegerische Vergangenheit sowie die terroristischen mandalorianischen Organisationen und der Machthunger der Mandalorianer. Sie thematisiert das Dilemma der Seperatisten und die Machtstrukturen der Mandalorianer sowie deren Verbissenheit.
+            Die erste animierte Serie im Star Wars Universium erklärt politische Zusammenhänge bezüglich Mandalore und die kriegerische Vergangenheit sowie die terroristischen mandalorianischen Organisationen und der Machthunger der Mandalorianer. Hinzukommt, dass der Zuschauer die Korruption im Senat und auf Mandalore zu Gesicht bekommt. Sie thematisiert das Dilemma der Seperatisten und die Machtstrukturen der Mandalorianer sowie deren Verbissenheit. Außerdem sehen wir wie die Deathwatch von Count Dooku und später von Darth Maul benutzt werden.
           </span>
         </article>
         <article class="card flex-column bg-gray-700 gray-300">
@@ -227,16 +229,19 @@
           </span>
         </article>
         <article class="card flex-column bg-gray-700 gray-300">
+          <div>
+            <img class="image" src="assets/images/mando.jpg" alt="mandalorian">
+            <img class="image" src="assets/images/blacksmith.jpg" alt="mandalorian-blacksmith">
+          </div>
           <span>
             Die preisgekrönte Disney+ Serie <strong>The Mandalorian</strong> thematisiert nicht nur die Findling Tradition der Mandalorianer sondern auch die unterschiedlichen Extremistischen Gruppierungen innerhalb der Mandalorianer. Auch hier wird das Darksaber und dessen Relevanz für das Volk der Mandalorianer angesprochen. Zudem bekommen wir zu Gesicht wie die verbleibenden Mandalorianer sich vor den Resten des Imperiums und der neuen Republik verstecken und ihren Lebensunterhalt als Mitglieder in Kopfgeldjägergilden verdienen. Wir lernen in dieser Serie Mandalorianische Traditionen, Schmiedehandwerk und Regeln kennen.
-          </span class="image" src="assets/images/mando.jpg" alt="mandalorian">
-          <img class="image" src="assets/images/blacksmith.jpg" alt="mandalorian-blacksmith">
+          </span>
         </article>
         <article class="card bg-gray-700 gray-300">
           <img class="image" src="assets/images/bobafett.jpg" alt="boba fett">
           <span>
             Das Book of Boba Fett handelt grundsätzlich von Boba Fetts Geschichte, d.h. wie er den Sarlac überlebte und mit den Sandmenschen zusammenlebte.
-            Wirklich relevant für die Mandalorianische Geschichte sind hier nur Ausschnitte, wie Bobas Rüstung, das für Mandalorianer typische Jagen und Zähmen von wilden Tieren und Kampfszenen die bestimmte Gadgets darstellen.
+            Wirklich relevant für die Mandalorianische Geschichte sind hier nur Ausschnitte, wie Bobas Rüstung, das für Mandalorianer typische Jagen und Zähmen von wilden Tieren und Kampfszenen die bestimmte Gadgets darstellen, wie z.B.: Whistling Arrows, JetPacks und Thermaldetonatoren.
           </span>
         </article>
       </div>
